be9ab31f15763fa29ffe3e23358dd830d89452d0
Agwanyanjaba <bonkewycliffe@yahoo.com>
2023-11-07 19:44:32 +0300

    initial commit

---
+++ src/main/java/com/crd/msreconciliation/MsreconciliationApplication.java
@@ -0,0 +1,13 @@
+package com.crd.msreconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsreconciliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsreconciliationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crd/msreconciliation/MsreconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crd.msreconciliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsreconciliationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
